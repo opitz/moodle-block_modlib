bd3d6977f2300bdc67d764a574af61c347f83822
Matthias Opitz <opitz@gmx.de>
2019-10-16 16:56:12 +0100

    works! but still needs backup files to be present...

--- amd/src/install_module.js
+++ amd/src/install_module.js
@@ -4,25 +4,27 @@ define(['jquery', 'core/config', 'core/str', 'core/modal_factory', 'core/modal_e
 
 // ---------------------------------------------------------------------------------------------------------------------
             var sectionModules = function() {
-
                 $('.template_section').change(function() {
                     var status = $(this).find('input').is(':checked');
                     var sectionId = $(this).attr('sectionid');
                     $('.template_module[sid = "' + sectionId + '"]').prop('checked', status);
-//                    $('.template_module[sid = "' + sectionId + '"]').click();
 
                     if ($('input:checked').length !== 0) {
-//                        $('#target_topic_btn').hide();
-//                        $('#after_topic_btn').show();
-//                        $('#after_topic_btn').removeClass('disabled');
-                        $('input.template_module').attr('disabled','');
+                        $('input.template_module').attr('disabled',''); // Disable module selection as they are part of a section
                         $('#target_topic_btn').removeClass('disabled');
+                        // Change button to section text
+                        var getTheString = str.get_string('select_section_after', 'block_modlib');
+                        $.when(getTheString).done(function(theString) {
+                            $('#target_topic_btn').html(theString);
+                        });
                     } else {
-//                        $('#target_topic_btn').show();
-//                        $('#after_topic_btn').hide();
-//                        $('#after_topic_btn').addClass('disabled');
-                        $('input.template_module').removeAttr('disabled');
+                        $('input.template_module').removeAttr('disabled'); // Activate module selection
                         $('#target_topic_btn').addClass('disabled');
+                        // Change button to module text
+                        var getTheString = str.get_string('select_section', 'block_modlib');
+                        $.when(getTheString).done(function(theString) {
+                            $('#target_topic_btn').html(theString);
+                        });
                     }
                 });
 
@@ -35,53 +37,22 @@ define(['jquery', 'core/config', 'core/str', 'core/modal_factory', 'core/modal_e
                 });
             };
 // ---------------------------------------------------------------------------------------------------------------------
-            var executeModules = function (self) {
-//                    alert('heda!');
+            var executeModules = function (sectionId) {
                 // Get the selected modules
-                $('.modlib-modal').show();
-//                    var sectionId = self.attr('value');
-//                    var count = $('input[class="template_module"]:checked').length;
                     $('input[class="template_module"]:checked').each(function () {
                         var module = {};
-//                        module.id = $(this).val();
-                        module.sectionid = self.attr('value');
+                        module.sectionid = sectionId;
                         module.cmid = $(this).attr('cmid');
                         module.name = $(this).attr('name');
                         module.type = $(this).attr('module_type');
 
                         callAjax(module);
-                        /*
-                        // Now install the module
-                        var execUrl = config.wwwroot + '/blocks/modlib/execute.php';
-                        $.ajax({
-                            url: execUrl,
-                            type: "POST",
-                            data: {
-                                'sectionid': sectionId,
-                                'cmid': module.cmid,
-                                'type': module.type
-                            },
-                            success: function (result) {
-                                $('#modlib-modal-msg').html(result);
-                                if (!--count) { // once all
-//                                            $('.modlib-modal').hide();
-                                    location.reload();
-                                }
-                            },
-                            error: function (e) {
-                                $('.modlib-modal').hide();
-                                console.error(e);
-                            }
-                        });
-                        */
                     });
             };
 
 // ---------------------------------------------------------------------------------------------------------------------
-            var executeSections = function(self) {
-                $('.modlib-modal').show(); // Show the graphical interlude...
-                var targetSectionId = self.attr('value');
-//                        var count = $('input[class="template_section"]:checked').length;
+            var executeSections = function(sectionId) {
+                // Get the selected sections
                 $('input[class="template_section"]:checked').each(function () {
                     var section = {};
                     section.id = $(this).attr('sid');
@@ -89,7 +60,7 @@ define(['jquery', 'core/config', 'core/str', 'core/modal_factory', 'core/modal_e
 
                     // Now install the module
                     var data = {};
-                    data.sectionid = targetSectionId;
+                    data.sectionid = sectionId;
                     data.cmid = section.id;
                     data.type = 'section';
 
@@ -97,76 +68,28 @@ define(['jquery', 'core/config', 'core/str', 'core/modal_factory', 'core/modal_e
                 });
             }
 // ---------------------------------------------------------------------------------------------------------------------
-            var execute0 = function () {
-                $(".modlib-sections .dropdown-item").on('click', function () {
-
-//                    $('.modlib-modal').show(); // Show the graphical interlude...
-
-                    // Get the selected sections
-                    if ($('input[class="template_section"]:checked').length === 0) {
-                        // No sections have been selected - on to single modules
-                        executeModules($(this));
-                    } else {
-                        $('.modlib-modal').show(); // Show the graphical interlude...
-                        var targetSectionId = $(this).attr('value');
-//                        var count = $('input[class="template_section"]:checked').length;
-                        $('input[class="template_section"]:checked').each(function () {
-                            var section = {};
-                            section.id = $(this).attr('sid');
-                            section.name = $(this).attr('name');
-
-                            // Now install the module
-                            var data = {};
-                            data.sectionid = targetSectionId;
-                            data.cmid = section.id;
-                            data.type = 'section';
-
-                            callAjax(data);
-                            /*
-                            var execUrl = config.wwwroot + '/blocks/modlib/execute.php';
-                            $.ajax({
-                                url: execUrl,
-                                type: "POST",
-                                data: {
-                                    'sectionid': targetSectionId,
-                                    'cmid': section.id,
-                                    'type': 'section'
-                                },
-                                success: function (result) {
-                                    $('#modlib-modal-msg').html(result);
-                                    if (!--count) { // once all
-//                                            $('.modlib-modal').hide();
-                                        location.reload();
-                                    }
-                                },
-                                error: function (e) {
-                                    $('.modlib-modal').hide();
-                                    console.error(e);
-                                }
-                            });
-                            */
-                        });
-                    }
-                });
-            };
             var execute = function () {
                 $(".modlib-sections .dropdown-item").on('click', function () {
 
-//                    $('.modlib-modal').show(); // Show the graphical interlude...
+                    $('.modlib-modal').show(); // Show the graphical interlude...
 
                     // Get the selected sections
                     if ($('input[class="template_section"]:checked').length === 0) {
                         // No sections have been selected - on to single modules
-                        executeModules($(this));
+                        executeModules($(this).attr('value'));
                     } else {
-                        executeSections($(this));
+                        executeSections($(this).attr('value'));
                     }
                 });
             };
 
 // ---------------------------------------------------------------------------------------------------------------------
             var callAjax = function(data) {
-                var count = $('input[class="template_section"]:checked').length;
+                if(data.type == 'section') {
+                    var count = $('input[class="template_section"]:checked').length;
+                } else {
+                    var count = $('input[class="template_module"]:checked').length;
+                }
                 var execUrl = config.wwwroot + '/blocks/modlib/execute.php';
                 $.ajax({
                     url: execUrl,
@@ -192,7 +115,6 @@ define(['jquery', 'core/config', 'core/str', 'core/modal_factory', 'core/modal_e
 // ---------------------------------------------------------------------------------------------------------------------
             var initFunctions = function () {
                 // Load all required functions above
-
                 sectionModules();
                 execute();
             };
--- execute.php
+++ execute.php
@@ -7,7 +7,7 @@ $cmid = $_POST['cmid'];
 $type = $_POST['type'];
 
 if($type == 'section') {
-    echo install_section($sectionid, $cmid);
+    echo install_section($sectionid, $cmid, $type);
 } else {
     echo install_module($sectionid, $cmid, $type);
 }
@@ -26,15 +26,12 @@ function add_section($course, $t_section) {
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-function install_section($target_sid, $template_sid) {
+function install_section($target_sid, $template_sid, $type) {
     global $CFG, $DB, $USER;
 
     require_once($CFG->dirroot . '/backup/util/includes/backup_includes.php');
     require_once($CFG->dirroot . '/backup/util/includes/restore_includes.php');
     require_once($CFG->libdir . '/filelib.php');
-
-    // get target course from sectionid
-//    $courseid = $DB->get_field('course_sections', 'course', array('id' => $target_sid));
 
     // get the section after which the new section will be installed
     $pre_section = $DB->get_record('course_sections', array('id' => $target_sid));
@@ -93,154 +90,10 @@ function install_section($target_sid, $template_sid) {
 
     rebuild_course_cache($courseid, true); // rebuild the cache for that course so the changes become effective
 
-    return 'Sections ID '.$template_sid.' will be installed after target section ID '.$target_sid.' here...';
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-function install_section_modules($targetsectionid, $sid) {
-    global $CFG, $DB, $USER;
-
-    require_once($CFG->dirroot . '/backup/util/includes/backup_includes.php');
-    require_once($CFG->dirroot . '/backup/util/includes/restore_includes.php');
-    require_once($CFG->libdir . '/filelib.php');
-
-    // get target course from sectionid
-    $courseid = $DB->get_field('course_sections', 'course', array('id' => $targetsectionid));
-    $course = $DB->get_record('course', array('id' => $courseid));
-
-    // Backup the section.
-    $bc = new backup_controller(backup::TYPE_1SECTION, $sid, backup::FORMAT_MOODLE,
-        backup::INTERACTIVE_NO, backup::MODE_GENERAL, $USER->id);
-
-    $backupid       = $bc->get_backupid();
-    $backupbasepath = $bc->get_plan()->get_basepath();
-
-    $bc->execute_plan();
-    $bc->destroy();
-
-    // Now restore the section after the given target
-    $targetsection = $DB->get_record('course_sections', array('id' => $targetsectionid));
-    $sectionnum = $targetsection->section;
-
-    // Restore the backup immediately.
-    $rc = new restore_controller($backupid, $course->id,
-        backup::INTERACTIVE_NO, backup::MODE_GENERAL, $USER->id, backup::TARGET_EXISTING_ADDING);
-
-    // Make sure that the restore_general_groups setting is always enabled when duplicating an activity.
-    $the_tasks = $rc->get_plan()->get_tasks();
-    foreach ($rc->get_plan()->get_tasks() as $task) {
-        if ($task->setting_exists('overwrite_conf'))
-            $task->get_setting('overwrite_conf')->set_value(false);
-    }
-
-    $rc->set_status(\backup::STATUS_AWAITING);
-    $rc->execute_plan();
-
-    // move the restored module to desired section
-    $the_tasks2 = $rc->get_plan()->get_tasks();
-
-    foreach ($rc->get_plan()->get_tasks() as $task) {
-        if ($task instanceof \restore_activity_task) {
-            $cmid = $task->get_moduleid();
-            $cm = \get_coursemodule_from_id(null, $cmid, 0, false, MUST_EXIST);
-            \moveto_module($cm, $targetsection);
-        }
-    }
-
-    if (empty($CFG->keeptempdirectoriesonbackup)) {
-        fulldelete($backupbasepath);
-    }
-    $rc->destroy();
-
-    rebuild_course_cache($courseid, true); // rebuild the cache for that course so the changes become effective
-
-    return 'Sections ID '.$sid.' will be installed after target section ID '.$targetsectionid.' here...';
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-function install_module1($sectionid, $cmid, $type) {
-    global $CFG, $DB, $USER;
-
-    require_once($CFG->dirroot . '/backup/util/includes/backup_includes.php');
-    require_once($CFG->dirroot . '/backup/util/includes/restore_includes.php');
-    require_once($CFG->libdir . '/filelib.php');
-
-    // get course from sectionid
-    $courseid = $DB->get_field('course_sections', 'course', array('id' => $sectionid));
-    $course = $DB->get_record('course', array('id' => $courseid));
-
-    // Backup the activity.
-    $bc = new backup_controller(backup::TYPE_1ACTIVITY, $cmid, backup::FORMAT_MOODLE,
-        backup::INTERACTIVE_NO, backup::MODE_GENERAL, $USER->id);
-
-    $backupid       = $bc->get_backupid();
-    $backupbasepath = $bc->get_plan()->get_basepath();
-
-    $bc->execute_plan();
-    $bc->destroy();
-
-    // Restore the backup immediately.
-    $rc = new restore_controller($backupid, $course->id,
-        backup::INTERACTIVE_NO, backup::MODE_GENERAL, $USER->id, backup::TARGET_CURRENT_ADDING);
-
-    // Make sure that the restore_general_groups setting is always enabled when duplicating an activity.
-    $plan = $rc->get_plan();
-    $groupsetting = $plan->get_setting('groups');
-    if (empty($groupsetting->get_value())) {
-        $groupsetting->set_value(true);
-    }
-
-    $cmcontext = context_module::instance($cmid);
-    if (!$rc->execute_precheck()) {
-        $precheckresults = $rc->get_precheck_results();
-        if (is_array($precheckresults) && !empty($precheckresults['errors'])) {
-            if (empty($CFG->keeptempdirectoriesonbackup)) {
-                fulldelete($backupbasepath);
-            }
-        }
-    }
-
-    $rc->execute_plan();
-/*
-    // Now a bit hacky part follows - we try to get the cmid of the newly
-    // restored copy of the module.
-    $newcmid = null;
-    $tasks = $rc->get_plan()->get_tasks();
-    foreach ($tasks as $task) {
-        if (is_subclass_of($task, 'restore_activity_task')) {
-            if ($task->get_old_contextid() == $cmcontext->id) {
-                $newcmid = $task->get_moduleid();
-                break;
-            }
-        }
-    }
-*/
-    $rc->destroy();
-
-    if (empty($CFG->keeptempdirectoriesonbackup)) {
-        fulldelete($backupbasepath);
-    }
-/*
-    if ($newcmid) {
-        // move the module to the destination section
-        $newcm = get_coursemodule_from_id($type, $newcmid);
-        $section = $DB->get_record('course_sections', array('id' => $sectionid));
-        moveto_module($newcm, $section);
-
-        // Update calendar events with the duplicated module.
-        // The following line is to be removed in MDL-58906.
-        course_module_update_calendar_events($newcm->modname, null, $newcm);
-
-        // Trigger course module created event. We can trigger the event only if we know the newcmid.
-        $newcm = get_fast_modinfo($course)->get_cm($newcmid);
-        $event = \core\event\course_module_created::create_from_cm($newcm);
-        $event->trigger();
-    }
-*/
-    rebuild_course_cache($courseid, true); // rebuild the cache for that course so the changes become effective
-
     return get_string('installed', 'block_modlib', ucfirst($type));
 }
+
+//----------------------------------------------------------------------------------------------------------------------
 function install_module($sectionid, $cmid, $type) {
     global $CFG, $DB, $USER;
 
@@ -324,5 +177,3 @@ function install_module($sectionid, $cmid, $type) {
 
     return get_string('installed', 'block_modlib', ucfirst($type));
 }
-
-
